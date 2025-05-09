242da4f40092ce8f348a7f854edbe17930b36737
Atauine Dias <atauine@roko.com.br>
2025-05-09 08:56:54 -0300

    Adiciona README e código do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Teste Técnico - Desenvolvedor 2
+
+Este repositório contém um projeto em **C# (Console Application)** desenvolvido como parte de um teste técnico.
+
+
+
+## ?? Como Executar o Projeto
+
+1. **Clone este repositório:**
+   ```bash
+   git clone https://github.com/seu-usuario/seu-repositorio.git
+
+   Iniciar a aplicação no Visual Studio ou via terminal com `dotnet run`.
--- Testes/obj/Debug/net9.0/Testes.AssemblyInfo.cs
+++ Testes/obj/Debug/net9.0/Testes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93809e698d218cffed367cc36834f03c011537ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
